898c01984cfa9fb2db8b3534a17e8639daea4fc6
Rune Goksør <57212482+rungok@users.noreply.github.com>
2026-09-16 14:40:58 +0200

    Update logo dimensions in PowerShell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,8 +3,8 @@ $tit = 'Pimped PowerShell-Profile for Windows v3.0 by GOKS0R'			 							#
 $githubUser = 'rungok'																				#
 $FFConfig = Join-Path -Path $env:localappdata -ChildPath 'fastfetch\frames.jsonc' # Config-path     #
 $FFlogo = Join-Path -Path $env:localappdata -ChildPath 'fastfetch\indianai_cropped2.png' # logopath #
-$FFlogoWidth = 60  # Width  in number of chars														#
-$FFlogoHeight = 42 # Height in number of chars														#
+$FFlogoWidth = 60  # Width  in number of chars                                                      #
+$FFlogoHeight = 42 # Height in number of chars                                                      #
 #																									#
 #  This script will try to install Windows Terminal (even on Windows Server 2022),					#
 #  nice ANSI-prompt and other enhancments/alias so even some Linux-commands will work.				#
